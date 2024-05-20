c97111480bc8d8a76668e9da03512c372239ebf8
irobertcostin <irobertcostin@gmail.com>
2024-05-20 09:06:27 +0300

    personal info update

--- src/components/user/MyAccount.jsx
+++ src/components/user/MyAccount.jsx
@@ -1,15 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser } from "../redux-store/user/User.selector";
 import PersonalInfo from "./PersonalInfo";
-import Orders from "./Orders";
+import Orders from "./orders/Orders";
 import { useState } from "react";
 import { resetState } from "../redux-store/user/User.reducer";
+import { HomeIcon } from "@heroicons/react/20/solid";
+import { useNavigate } from "react-router-dom";
 
 
 export default function MyAccount() {
 
     const user = useSelector(selectUser);
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const INFO = "info";
     const ORDERS = "orders";
@@ -20,7 +23,7 @@ export default function MyAccount() {
 
 
     const actions = [
-        { name: "Cont", key: INFO },
+        { name: "Info", key: INFO },
         { name: "Comenzi", key: ORDERS }
     ];
 
@@ -29,9 +32,13 @@ export default function MyAccount() {
     }
 
     return (
-        <div className="w-full text-slate-400 py-20 flex flex-col items-center relative sm:px-10">
+        <div className="w-full  flex flex-col items-center relative sm:px-10 lg:py-20 ">
+            <div className="py-10 w-full text-start text-2xl lg:text-3xl font-semibold text-white px-5 sm:max-w-5xl flex justify-between">
+                <h1 className="">Contul meu</h1>
+                <HomeIcon onClick={() => { navigate('/') }} className="w-8 text-gray-400 hover:text-white cursor-pointer lg:hidden" />
+            </div>
             <div className="w-full flex px-5 sm:justify-center sm:max-w-5xl">
-                <div className="w-full flex justify-start items-center text-lg  font-bold gap-10 max-w-5xl ">
+                <div className="w-full flex justify-start items-center text-lg text-gray-400  font-semibold gap-5 sm:gap-10 max-w-5xl ">
                     {actions.map((item) => (
                         <button
                             key={item.key}
@@ -39,13 +46,12 @@ export default function MyAccount() {
                                 setIsActive(item.key);
                                 setPage(item.key);
                             }}
-                            className={`${isActive === item.key ? "text-amber-400" : ""} hover:text-amber-400`}
+                            className={`${isActive === item.key ? "text-amber-400" : ""} hover:text-amber-400 lg:text-2xl`}
                         >
                             {item.name}
                         </button>
                     ))}
                 </div>
-
                 <button
                     onClick={signOut}
                     className="bg-amber-400 hover:bg-amber-300 text-black flex w-52 justify-center rounded-sm  px-3 py-1 text-sm font-semibold leading-6 "
@@ -53,7 +59,6 @@ export default function MyAccount() {
                     Delogheaza-te
                 </button>
             </div>
-
             <div className="w-full flex justify-center ">
                 {(() => {
                     switch (page) {
