86c212db5679b2a1ef958b5cdf64f255d595166e
irobertcostin <irobertcostin@gmail.com>
2024-05-20 09:07:00 +0300

    shopping cart component

---
+++ src/components/user/ShoppingCart.jsx
@@ -0,0 +1,164 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { useNavigate } from 'react-router-dom'
+
+
+
+export default function ShoppingCart({ open, setOpen }) {
+
+    const navigate = useNavigate()
+
+
+    const products = [
+        {
+            id: 1,
+            name: 'Throwback Hip Bag',
+            href: '#',
+            color: 'Salmon',
+            price: 'LEI 90.00',
+            quantity: 1,
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+            imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+        },
+        {
+            id: 2,
+            name: 'Medium Stuff Satchel',
+            href: '#',
+            color: 'Blue',
+            price: 'LEI 32.00',
+            quantity: 1,
+            imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+            imageAlt:
+                'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+        },
+    ]
+
+    return (
+        <Transition.Root show={open} as={Fragment}>
+            <Dialog className="relative z-50" onClose={setOpen}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-500"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-80 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-hidden">
+                    <div className="absolute inset-0 overflow-hidden">
+                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                                enterFrom="translate-x-full"
+                                enterTo="translate-x-0"
+                                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                                leaveFrom="translate-x-0"
+                                leaveTo="translate-x-full"
+                            >
+                                <Dialog.Panel className="pointer-events-auto w-screen max-w-lg">
+                                    <div className="flex h-full flex-col overflow-y-scroll bg-black shadow-xl px-2">
+                                        <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                                            <div className="flex items-start justify-between">
+                                                <Dialog.Title className="text-lg font-medium text-white">Cart cumparaturi</Dialog.Title>
+                                                <div className="ml-3 flex h-7 items-center">
+                                                    <button
+                                                        type="button"
+                                                        className="relative -m-2 p-2 text-gray-300 hover:text-white"
+                                                        onClick={() => setOpen(false)}
+                                                    >
+                                                        <span className="absolute -inset-0.5" />
+                                                        <span className="sr-only">Close panel</span>
+                                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                            <div className="mt-8">
+                                                <div className="flow-root">
+                                                    <ul role="list" className="-my-6 divide-y divide-gray-700">
+                                                        {products.map((product) => (
+                                                            <li key={product.id} className="flex py-6">
+                                                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                                                    <img
+                                                                        src={product.imageSrc}
+                                                                        alt={product.imageAlt}
+                                                                        className="h-full w-full object-cover object-center"
+                                                                    />
+                                                                </div>
+
+                                                                <div className="ml-4 flex flex-1 flex-col">
+                                                                    <div>
+                                                                        <div className="flex justify-between text-base font-medium text-gray-200">
+                                                                            <h3>
+                                                                                <a href={product.href}>{product.name}</a>
+                                                                            </h3>
+                                                                            <p className="ml-4">{product.price}</p>
+                                                                        </div>
+                                                                        <p className="mt-1 text-sm text-gray-400">{product.color}</p>
+                                                                    </div>
+                                                                    <div className="flex flex-1 items-end justify-end text-sm">
+                                                                        <div className="flex">
+                                                                            <button
+                                                                                type="button"
+                                                                                className="font-medium text-gray-300 "
+                                                                            >
+                                                                                <TrashIcon className='w-6' />
+                                                                            </button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                                            <div className="flex justify-between text-base font-medium text-gray-200">
+                                                <p>Total</p>
+                                                <p>LEI 262.00</p>
+                                            </div>
+                                            <p className="mt-0.5 text-sm text-gray-400">Taxele sunt incluse in pretul afisat.</p>
+                                            <div className="mt-6">
+                                                <a
+                                                    href="#"
+                                                    className="flex items-center justify-center rounded-md border border-transparent bg-amber-400 px-6 py-3 text-base font-medium text-black shadow-sm hover:bg-amber-300"
+                                                >
+                                                    Checkout
+                                                </a>
+                                            </div>
+                                            <div className="mt-6 flex justify-center text-center text-sm text-gray-400">
+                                                <p>
+                                                    sau{' '}
+                                                    <button
+                                                        type="button"
+                                                        className="font-semibold text-gray-200 ml-2"
+                                                        onClick={() => {
+                                                            navigate('/cursuri')
+                                                            setOpen(false)
+                                                        }
+                                                        }
+                                                    >
+                                                        Continua cumparaturile
+                                                        <span aria-hidden="true"> &rarr;</span>
+                                                    </button>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
